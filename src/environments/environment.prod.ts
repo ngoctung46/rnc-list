1f0aef8af82f83f1e9a083bdb7e318e580e4c96c
ngoctung46 <ngoctung46@github.com>
2019-06-25 20:41:14 +0700

    fix error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB0twx3ALDoxQSQvQZsashgAQof1gMZowM',
+    authDomain: 'rncfinderapp.firebaseapp.com',
+    databaseURL: 'https://rncfinderapp.firebaseio.com',
+    projectId: 'rncfinderapp',
+    storageBucket: 'rncfinderapp.appspot.com',
+    messagingSenderId: '1003323466155',
+    appId: '1:1003323466155:web:0938f011a3f880bd'
+  }
 };
